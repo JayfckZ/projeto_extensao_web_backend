ae5e5847cd2dae2a315af67d4eeef180a6d90aff
JayfckZ <joaomarcelo1015ja@gmail.com>
2025-09-09 20:55:34 -0300

    testando api

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 # Inicializa a aplicação Flask
 app = Flask(__name__)
+CORS(app)
 
 # Simulação de banco de dados em memória (lista de projetos)
 projects = []
@@ -13,13 +15,15 @@ def get_route():
     return jsonify(projects), 200
 
 
-# ROta para adicionar um novo mprojeto (POST)
+# Rota para adicionar um novo mprojeto (POST)
 @app.route("/projects", methods=["POST"])
 def add_project():
-    new_project = request.get_json()  # Pega os dados enviados no corpo da requisição
-    projects.append(new_project)
-    return jsonify(new_project), 201
-
+    try:
+        new_project = request.get_json()  # Pega os dados enviados no corpo da requisição
+        projects.append(new_project)
+        return jsonify(new_project), 201
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
 
 # Ponto de entrada para rodar a aplicação
 if __name__ == "__main__":
