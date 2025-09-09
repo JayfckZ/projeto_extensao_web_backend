5aa3393bc4ee2b06f2d8cec0ec789e0402b5e6fe
JayfckZ <joaomarcelo1015ja@gmail.com>
2025-09-09 19:55:37 -0300

    init frontend + docker-compose

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+import React from 'React'
+
+export default function App() {
+    return (
+        <div>
+            <h1>Bem-vindo à aplicação de exemplo!</h1>
+            <p>Este é um exemplo de uma Single Page Applicantion (SPA) usando React.</p>
+        </div>
+    )
+}
